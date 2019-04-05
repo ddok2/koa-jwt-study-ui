958818a61e52863814d548e9dd014149fe8a2f81
SUNGYUB NA <darkerkorean@gmail.com>
2019-04-05 09:00:33 +0900

    헤더 구성하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Router } from 'react-router-dom';
 import { Home, Auth } from 'pages';
+import HeaderContainer from './components/Base/HeaderContainer';
 
 class App {
     render() {
         return (
             <div>
+                <HeaderContainer/>
                 <Router exact path="/" component={Home}/>
                 <Router path="/auth" component={Auth}/>
             </div>
---
+++ src/components/Base/HeaderContainer.js
@@ -0,0 +1,14 @@
+import React, { Compenent } from 'react';
+import Header from './Header';
+
+class HeaderContainer {
+    render() {
+        return (
+            <Header>
+                Hello World, Sung
+            </Header>
+        );
+    }
+}
+
+export default HeaderContainer;
