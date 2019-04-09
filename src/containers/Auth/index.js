7ad7440c71400b34c5ac1071ed090e7c74c808ea
SUNGYUB NA <darkerkorean@gmail.com>
2019-04-10 08:53:28 +0900

    회원인증 폼 구성하기

--- src/containers/Auth/index.js
+++ src/containers/Auth/index.js
@@ -1,2 +1,4 @@
-export { default as Login } from './Login';
-export { default as Register } from './Register';
+export { default as AuthWrapper } from './AuthWrapper';
+export { default as AuthContent } from './AuthContent';
+export { default as InputWithLabel } from './InputWithLabel';
+export { default as AuthButton } from './AuthButton';
