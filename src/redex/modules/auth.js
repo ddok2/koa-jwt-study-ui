4ecf476a61be7f37a306ceb85242566d09093f1a
SUNGYUB NA <darkerkorean@gmail.com>
2019-04-14 17:28:04 +0900

    리덕스 액션 준비

--- src/redex/modules/auth.js
+++ src/redex/modules/auth.js
@@ -1,12 +1,29 @@
 import { createAction, handleActions } from 'redux-actions';
+import { pender } from 'redux-pender';
+import * as AuthAPI from 'lib/api/auth';
 
 import { Map } from 'immutable';
 
 const CHANGE_INPUT = 'auth/CHANGE_INPUT';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+const CHECK_EMAIL_EXISTS = 'auth/CHECK_EMAIL_EXISTS';
+const CHECK_USERNAME_EXISTS = 'auth/CHECK_USERNAME_EXISTS';
+const LOCAL_REGISTER = 'auth/LOCAL_REGISTER';
+const LOCAL_LOGIN = 'auth/LOCAL_LOGIN';
+const LOGOUT = 'auth/LOGOUT';
 
 export const changeInput = createAction(CHANGE_INPUT);
 export const initializeForm = createAction(INITIALIZE_FORM);
+export const checkEmailExists = createAction(CHECK_EMAIL_EXISTS,
+    AuthAPI.checkEmailExists);
+export const checkUsernameExists = createAction(CHECK_USERNAME_EXISTS,
+    AuthAPI.checkUsernameExists);
+
+export const localRegister = createAction(LOCAL_REGISTER,
+    AuthAPI.localRegister);
+export const localLogin = createAction(LOCAL_LOGIN, AuthAPI.localLogin);
+
+export const logout = createAction(LOGOUT, AuthAPI.logout);
 
 const initialState = Map({
     register: Map({
@@ -16,6 +33,10 @@ const initialState = Map({
             password: '',
             passwordConfirm: '',
         }),
+        exists: Map({
+            email: false,
+            password: false,
+        }),
     }),
     login: Map({
         form: Map({
@@ -23,6 +44,7 @@ const initialState = Map({
             password: '',
         }),
     }),
+    result: Map({}),
 });
 
 export default handleActions({
@@ -34,4 +56,24 @@ export default handleActions({
         const initialForm = initialState.get(action.payload);
         return state.set(action.payload, initialForm);
     },
+    ...pender({
+        type: CHECK_EMAIL_EXISTS,
+        onSuccess: (state, action) => state.setIn(
+            ['register', 'exists', 'email'], action.payload.data.exists),
+    }),
+    ...pender({
+        type: CHECK_USERNAME_EXISTS,
+        onSuccess: (state, action) => state.setIn(
+            ['register', 'exists', 'username'], action.payload.data.exists),
+    }),
+    ...pender({
+        type: LOCAL_LOGIN,
+        onSuccess: (state, action) => state.set('result',
+            Map(action.payload.data)),
+    }),
+    ...pender({
+        type: LOCAL_REGISTER,
+        onSuccess: (state, action) => state.set('result',
+            Map(action.payload.data)),
+    }),
 }, initialState);
