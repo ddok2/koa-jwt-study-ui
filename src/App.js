ace0a4b2f78d3ed7d1c76496c8a6132c0297c298
SUNGYUB NA <darkerkorean@gmail.com>
2019-04-24 08:52:30 +0900

    새로고침시 로그인 유지하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import { Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { Home, Auth } from 'pages';
-import HeaderContainer from './components/Base/HeaderContainer';
+import HeaderContainer from 'containers/Base/HeaderContainer';
 
-class App {
+import storage from 'lib/storage';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as userActions from 'redux/modules/user';
+
+class App extends Component {
+
+    initializeUserInfo = async () => {
+        const loggedInfo = storage.get('loggedInfo');
+        if (!loggedInfo) return;
+
+        const { UserActions } = this.props;
+        UserActions.setLoggedInfo(loggedInfo);
+        try {
+            await UserActions.checkStatus();
+        } catch (e) {
+            storage.remove('loggedInfo');
+            window.location.href = '/auth/login?expired';
+        }
+    };
+
+    componentDidMount() {
+        this.initializeUserInfo();
+    }
+
     render() {
         return (
             <div>
                 <HeaderContainer/>
-                <Router exact path="/" component={Home}/>
-                <Router path="/auth" component={Auth}/>
+                <Route exact path="/" component={Home}/>
+                <Route path="/auth" component={Auth}/>
             </div>
         );
     }
 }
 
-export default App;
+export default connect(
+    null,
+    (dispatch) => ({
+        UserActions: bindActionCreators(userActions, dispatch),
+    }),
+)(App);
